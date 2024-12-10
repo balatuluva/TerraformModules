c5231bf5bc5e7c7c6b68e8af55542cd60371b80a
balatuluva <bala.tulva@gmail.com>
2024-12-10 06:40:55 +0000

    Dev and Prod modules setup with sg changes

--- production/infra.tf
+++ production/infra.tf
@@ -7,3 +7,11 @@ module "prod_vpc" {
   private_cidr_block = ["192.168.10.0/24", "192.168.20.0/24", "192.168.30.0/24"]
   environment        = "Production"
 }
+
+module "prod_sg" {
+  source = "../modules/sg"
+  vpc_name = module.prod_vpc.vpc_name
+  vpc_id = module.prod_vpc.vpc_id
+  ingress_service = ["80", "8080", "443", "8443", "22", "3306", "1900", "1443"]
+  environment = module.prod_vpc.environment
+}
